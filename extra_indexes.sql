a2878995097e5a7e5c4e958e47eba96de035af0b
John McCrae <john@mccr.ae>
2014-07-30 18:07:59 +0200

    Clean branch

--- extra_indexes.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX IF NOT EXISTS k_morphmaps_wordid ON morphmaps (wordid);
-CREATE INDEX IF NOT EXISTS k_morphmaps_morphid ON morphmaps (morphid);
-CREATE INDEX IF NOT EXISTS k_phrasetypes_synsetid on [phrasetypes] ( synsetid );
-CREATE INDEX IF NOT EXISTS k_morphs_morphid on [morphs] (morphid);
-CREATE INDEX IF NOT EXISTS [idx_sensetags_new_sensekey] on [sensetags] ( [new_sensekey] );
-CREATE TABLE [synsettriples] ([synsetid] INTEGER NOT NULL REFERENCES [synsets] ([synsetid]), property VARCHAR(80) NOT NULL, object VARCHAR(80) NOT NULL);
-CREATE INDEX k_synsettriples_synsetid ON [synsettriples] ( synsetid );
-CREATE TABLE [entrytriples] ([lemma] VARCHAR(80), [fragment] VARCHAR(80), property VARCHAR(80) NOT NULL, object VARCHAR(80) NOT NULL);
-CREATE INDEX k_entrytriples_lemma ON [entrytriples] ( lemma );
--- ontology-uris-fix.sql
+++
@@ -1,3 +0,0 @@
-update synsettriples set property="http://wordnet-rdf.princeton.edu/ontology#translation" where property="http://wordnet.princeton.edu/rdf/ontology#translation";
-update entrytriples set property="<http://wordnet-rdf.princeton.edu/ontology#verbnet_class>" where property="<http://wordnet.princeton.edu/rdf/ontology#verbnet_class>";
-
